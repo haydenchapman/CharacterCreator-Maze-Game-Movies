3f6438c25a83d2acc67bfc68d5716fcbbbfe5f70
Hayden Chapman <hayden.chapman@my.tccd.edu>
2019-08-26 18:56:17 -0500

    talked about variable types

--- classwork/Section1/HelloWorld/Program.cs
+++ classwork/Section1/HelloWorld/Program.cs
@@ -1,7 +1,6 @@
 ﻿/*
  * HI MY NAME IS
  * YOYOYOYOYOYOYOYOYOYO
- * 
  */
 
 using System;
@@ -11,9 +10,34 @@ namespace HelloWorld
     class Program
     {
 
-        static void Main ( string[] args )
+        static void Main ( /*string[] args*/ )
         {
+            string name = "";
+
+            //(string if = "";) cannot use keywords as ID
+
+            //Definitely assigned
+            //name = "Bob";
+
+            string name2 = Console.ReadLine ();
+            
+            Console.WriteLine (name2);
+
+            name2 = name = "Sue";
+            Console.WriteLine (name2);
             Console.WriteLine ("Hello World!");
+
+            //another block
+
+            int hours = 8;
+            double payRate = 15.25;
+
+            double totalPay = payRate * hours;
+
+            string fullName = "Fred" + " " + "Jones";
+
+            Console.WriteLine (fullName);
+
         }
 
     }
