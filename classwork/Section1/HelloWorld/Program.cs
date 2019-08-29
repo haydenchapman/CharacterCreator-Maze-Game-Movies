df00a85e4df5c1484bf178759c27d77ad5c1fe90
hayden.chapman <hayden.chapman@my.tccd.edu>
2019-08-28 19:13:18 -0500

    added read movie input.

--- classwork/Section1/HelloWorld/Program.cs
+++ classwork/Section1/HelloWorld/Program.cs
@@ -12,6 +12,106 @@ namespace HelloWorld
 
         static void Main ( /*string[] args*/ )
         {
+            //movie data
+            string title;
+            int runLength;
+            int releaseYear;
+            string description;
+            bool haveSeen;
+
+            while (true)
+            {
+                char input = DisplayMenu ();
+                if (input == 'A')
+                    AddMovie ();
+                else if (input == 'Q')
+                    break;
+            };
+        }
+
+        static void AddMovie ()
+        {
+            //Get title
+            Console.WriteLine ("Title: ");
+            string title = Console.ReadLine ();
+
+            //Get description
+            Console.WriteLine ("Description: ");
+            string description = Console.ReadLine ();
+
+            //Get release year
+            int releaseYear = ReadInt32 ("Release year: ");
+
+            //Get run length
+            int runLength = ReadInt32 ("Run length (in minutes): ");
+
+            //Get have seen
+            bool haveSeen = ReadBoolean ("Have seen? ");
+        }
+
+        static int ReadInt32 ( string message )
+        {
+            while (true)
+            {
+                Console.WriteLine (message);
+
+                string input = Console.ReadLine ();
+
+                //int result = Int32.Parse (input);
+                //int result;
+                //if (Int32.TryParse (input, out result))
+                if (Int32.TryParse (input, out int result))
+
+                    return result;
+
+                Console.WriteLine ("Not a number");
+            }
+        }
+
+        static bool ReadBoolean ( string message )
+        {
+            while (true)
+            {
+                Console.WriteLine (message);
+
+                string input = Console.ReadLine ();
+
+                bool result;
+                if (Boolean.TryParse (input, out result))
+
+                    return result;
+
+                Console.WriteLine ("Not a boolean");
+            }
+        }
+
+        static char DisplayMenu ()
+        {
+            do
+            {
+                Console.WriteLine ("A)dd Movie");
+                Console.WriteLine ("Q)uit");
+
+                string input = Console.ReadLine ();
+
+                if (input == "A" || input == "a")
+                {
+                    return 'A';
+                } else if (input == "Q" || input == "q")
+                {
+                    return 'Q';
+                } else
+                {
+                    Console.WriteLine ("Invalid input");
+                };
+
+            } while (true);
+        }
+
+
+
+        private static string DemoLanguage ()
+        {
             string name = "";
 
             //(string if = "";) cannot use keywords as ID
@@ -20,7 +120,7 @@ namespace HelloWorld
             //name = "Bob";
 
             string name2 = Console.ReadLine ();
-            
+
             Console.WriteLine (name2);
 
             name2 = name = "Sue";
@@ -35,10 +135,7 @@ namespace HelloWorld
             double totalPay = payRate * hours;
 
             string fullName = "Fred" + " " + "Jones";
-
-            Console.WriteLine (fullName);
-
+            return fullName;
         }
-
     }
 }
