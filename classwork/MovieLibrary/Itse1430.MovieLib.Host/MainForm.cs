3e152a7868a803efa63aeea04044e028851d180d
Hayden Chapman <hayden.chapman@my.tccd.edu>
2019-09-11 19:12:44 -0500

    Intro WinForms, Namespace, Architecture dependecies

---
+++ classwork/MovieLibrary/Itse1430.MovieLib.Host/MainForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace Itse1430.MovieLib.Host
+{
+    public partial class MainForm : Form
+    {
+        public MainForm ()
+        {
+            InitializeComponent ();
+
+            var movie = new Movie();
+            movie = new Movie();
+        }
+    }
+}
